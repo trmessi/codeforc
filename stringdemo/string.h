2536c4b3de7e29e301e8eac5c077241ae4cd455e
tr <1289380467@qq.com>
2022-04-24 21:55:29 +0800

    d

---
+++ stringdemo/string.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef __MYSTRING__
+#define __MYSTRING__
+class String
+{
+public:
+	String(const char* cstr = 0);
+	String(const String& str);
+	String& operator=(const String& str);//只要是带指针的类，就要带这两个构造函数
+	~String();
+	char* get_c_str() const { return m_data; }
+private:
+	char* m_data;
+};
+inline
+String::String(const char* cstr = 0)
+{
+	if (cstr)
+	{
+		m_data = new char[strlen(cstr) + 1];
+		strcpy(m_data, cstr);
+	}
+	else
+	{
+		m_data = new char[1];
+		*m_data = '\0';
+	}
+}
+inline
+String::~String()
+{
+	delete[]m_data;
+}
+inline
+String::String(const String& str)
+{
+	m_data = new char[strlen(str.m_data) + 1];
+	strcpy(m_data, str.m_data);
+}
+inline
+String& String::operator=(const String& str)
+{
+	if (this == &str)//重要，检测自我赋值，
+		return *this;
+	delete[] m_data;
+	m_data = new char[strlen(str.m_data) + 1];
+	strcpy(m_data, str.m_data);
+	return *this;
+}
+
+
+
+#endif // !1
---
+++ stringdemo/stringtest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+#include"string.h"
+using namespace std;
+int main()
+{
+	String s1();
+	String s2("hello");
+	String s3(s1);//拷贝构造函数
+	cout << s3 << endl;
+	s3 = s2;//赋值构造函数,如果是类带指针不能用编译器默认构造函数，要自己写。
+	cout << s3 << endl;
+
+}
