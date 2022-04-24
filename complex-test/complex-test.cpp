3f5bf5391142fc39eab819708d7a19661fff037a
tr <1289380467@qq.com>
2022-04-24 20:49:13 +0800

    go

--- complex-test/complex-test.cpp
+++ complex-test/complex-test.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	complex c1(2, 1);
 	complex c2;
-/*
+
 	cout << c1<<endl;
 	cout << c2 << endl;
 	c2 = c1 + 5;
@@ -15,9 +15,7 @@ int main()
 	c2 += c1;
 	c2 += 3;
 	c2 = -c1;
-	cout << (c1 == c2) << endl;
-	cout << (c1 != c2) << endl;
-	cout << conj(c1) << endl;
-	*/
+	cout << c1 << endl;
+	cout << c2 << endl;
 	return 0;
 }
--- complex-test/complex.h
+++ complex-test/complex.h
@@ -3,14 +3,15 @@
 #ifndef __COMPLEX__
 #define __COMPLEX__
 //防卫式声明以防头文件多次解析
-
-
+using namespace std;
+//相同class的各个objects互为friends
 class complex
 {
 public:
 	friend complex& __doapl(complex*, const complex&);
 	complex(double r = 0, double i = 0):re(r),im(i) {}
-	complex& operator +=(const complex);
+	
+	complex& operator +=(const complex&);
 	double real() const { return re; }//不会改变数据的函数就加const  
 	double imag() const { return im; }
 private:
@@ -29,10 +30,30 @@ __doapl(complex* ths, const complex& r)
 }
 //参数传递：尽量引用 pass by reference ,如果不希望改数据则const
 //返回值传递：尽量 return by reference.不能的情况：
-/*ostream&
-operator<<(ostream& os, const complex& x)
+
+//传递者无需知道接收者是以reference接收的
+inline complex&
+complex::operator +=(const complex& r)
 {
-	return os << '(' << real(x) << '.'
-		<< imag(x) << ')';
-}1
-*/
+	return __doapl(this, r);
+}
+inline complex//值传递会触发拷贝构造函数，引用不会。
+operator +(const complex& x, const complex& y)
+{
+	return complex(x.real() + y.real(), x.imag() + y.imag());//临时对象
+}
+inline const complex&
+operator + (const complex & x)
+{
+	return x;
+}
+inline complex
+operator - (const complex& x)
+{
+	return complex(-x.real(),-x.imag());
+}
+std::ostream&
+operator << (ostream& os, const complex& x)
+{
+	return os << '(' << x.real() << ',' << x.imag() << ')';
+}
