5de80b9ba0c18b6b24989f6464142d18df340bbb
tr <1289380467@qq.com>
2022-05-17 21:19:30 +0800

    合并二叉树

--- Project3/源.cpp
+++ Project3/源.cpp
@@ -159,8 +159,15 @@ struct TreeNode {
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
     
 };
-TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-    stack<std::pair<TreeNode, TreeNode>> mystack;
-    for(;)
-    mystack.push()
+TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+    if (t1 == nullptr) {
+        return t2;
+    }
+    if (t2 == nullptr) {
+        return t1;
+    }
+    auto merged = new TreeNode(t1->val + t2->val);
+    merged->left = mergeTrees(t1->left, t2->left);
+    merged->right = mergeTrees(t1->right, t2->right);
+    return merged;
 }
