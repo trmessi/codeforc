06c13dcaa802abd683022556c8a53faed3260035
tr <1289380467@qq.com>
2022-05-17 21:37:18 +0800

    填充每个节点的下一个右侧节点指针

--- Project3/源.cpp
+++ Project3/源.cpp
@@ -171,3 +171,43 @@ TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
     merged->right = mergeTrees(t1->right, t2->right);
     return merged;
 }
+//给定一个?完美二叉树?，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+
+struct Node {
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+}
+/*填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有?next 指针都被设置为 NULL。*/
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+Node* connect(Node* root) {
+    if (root == NULL) return NULL;
+    connection(root);
+    return root;
+}
+void connection(Node* root) {
+    if (root->left == NULL) return;
+    root->left->next = root->right;
+    if (root->next != NULL) {
+        root->right->next = root->next->left;
+    }
+    connection(root->left);
+    connection(root->right);
+}
